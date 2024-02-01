3baf46ae1e40b9a88d1a42a597e11753d20f6327
lcgraves <lgraves@northislandcollege.ca>
2024-02-01 14:09:42 -0800

    Fix toggle to show both parts of mobile menu

--- design-system.js
+++ design-system.js
@@ -1,4 +1,8 @@
+/* Toggles mobile menu on and off with click/tap on hamburger icon */
+
 function showNav() {
-    var element = document.getElementById("nav-items");
-    element.classList.toggle("show-items");
+    var firstMenu = document.getElementById("c-mobile-first-menu");
+    var secondMenu = document.getElementById("c-mobile-second-menu")
+    firstMenu.classList.toggle("h-show-items");
+    secondMenu.classList.toggle("h-show-items");
 }
