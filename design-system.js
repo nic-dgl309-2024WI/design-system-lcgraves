340579981c561125232e4176d087d1f3686427f2
lcgraves <lgraves@northislandcollege.ca>
2024-02-01 14:21:17 -0800

    Add explanation to javascript for toggle function

--- design-system.js
+++ design-system.js
@@ -1,4 +1,5 @@
 /* Toggles mobile menu on and off with click/tap on hamburger icon */
+/* Used chatGPT to help with figuring out how to toggle on/off two menus with separate ID's with one click event" */
 
 function showNav() {
     var firstMenu = document.getElementById("c-mobile-first-menu");
