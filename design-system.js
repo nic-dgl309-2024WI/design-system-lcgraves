f39f2d472a08e0e395f018a6b0f20785ab9966ff
lcgraves <lgraves@northislandcollege.ca>
2024-02-02 10:32:25 -0800

    Adjust class names and add comments

--- design-system.js
+++ design-system.js
@@ -2,8 +2,8 @@
 /* Used chatGPT to help with figuring out how to toggle on/off two menus with separate ID's with one click event" */
 
 function showNav() {
-    var firstMenu = document.getElementById("c-mobile-first-menu");
-    var secondMenu = document.getElementById("c-mobile-second-menu")
-    firstMenu.classList.toggle("h-show-items");
-    secondMenu.classList.toggle("h-show-items");
+    var firstMenu = document.getElementById("c-mobile-menu__first");
+    var secondMenu = document.getElementById("c-mobile-menu__second")
+    firstMenu.classList.toggle("is-visible-dropdown-menu");
+    secondMenu.classList.toggle("is-visible-dropdown-menu");
 }
