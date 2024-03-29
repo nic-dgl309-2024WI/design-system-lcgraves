d3bad94fa0325841f1dae14e2b2b1be063f33f96
lcgraves <lgraves@northislandcollege.ca>
2024-03-28 19:37:13 -0700

    Style nav and hero section for mobile

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // Banner text scrolling effect
-
 document.getElementById('scrollingText').addEventListener('click', function() {
     this.classList.toggle('paused');
   });
+
+  // Navigation
+
+document.getElementById("plants").addEventListener("click", showPlants);
+document.getElementById("hamburger").addEventListener("click", showNav);
+
+function showNav() {
+    var element = document.getElementById("nav-items");
+    element.classList.toggle("show-items");
+}
+
+function showPlants() {
+    var plantstuff = document.getElementById("plant-items");
+    plantstuff.classList.toggle("show-plants");
+}
