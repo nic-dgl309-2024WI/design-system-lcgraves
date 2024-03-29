7211013423fb063e2ef779c3a1d338fc30ed5644
lcgraves <lgraves@northislandcollege.ca>
2024-03-29 13:39:05 -0700

    Add functionality to accordion with javascript

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ function showPlants() {
     var plantstuff = document.getElementById("plant-items");
     plantstuff.classList.toggle("show-plants");
 }
+
+// Accordion
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all accordion buttons
+    var accButtons = document.querySelectorAll('.c-accordion__button');
+
+    // Iterate through each button
+    accButtons.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            // This variable will toggle between true and false
+            var panel = this.nextElementSibling;
+
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                // Close any already open panels
+                var openPanel = document.querySelector('.c-accordion__panel[style]');
+                if (openPanel) openPanel.style.maxHeight = null;
+
+                // Open the clicked panel
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    });
+});
