903ea184e7cf359447b13cfe74356ad8a8d313c5
lcgraves <lgraves@northislandcollege.ca>
2024-04-18 17:21:19 -0700

    Add show nav function back in after deleting accidentally

--- design-system.js
+++ design-system.js
@@ -1,6 +1,13 @@
 /* Toggles mobile menu on and off with click/tap on hamburger icon */
 /* Used chatGPT to help with figuring out how to toggle on/off two menus with separate ID's with one click event" */
 
+function showNav() {
+  var firstMenu = document.getElementById("c-mobile-menu__first");
+  var secondMenu = document.getElementById("c-mobile-menu__second")
+  firstMenu.classList.toggle("is-visible-dropdown-menu");
+  secondMenu.classList.toggle("is-visible-dropdown-menu");
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
